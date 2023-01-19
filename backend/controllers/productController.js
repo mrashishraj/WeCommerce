c49f8a8480b5c405c42fead160ccf14440b6e597
mrashishraj <mr.ashishrajyadav@gmail.com>
2023-01-19 13:15:05 +0530

    testing push

---
+++ backend/controllers/productController.js
@@ -0,0 +1,92 @@
+const Product = require("../models/productModels")
+const ErrorHandler = require("../utils/errorHandler")
+const catchAsyncErrors = require("../middleware/catchAsyncErrors")
+const ApiFeatures = require("../utils/apiFeatures")
+
+// ===========***** Create Product -- admin *****=============
+
+exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
+    req.body.user = req.user.id
+
+    const product = await Product.create(req.body)
+ 
+    res.status(201).json({
+        success:true,
+        product
+    })
+
+})
+
+// Get All Product
+
+exports.getAllProducts= catchAsyncErrors(async (req,res)=>{
+    const resultPerPage = 5
+    const ApiFeature = new ApiFeatures(Product.find(),req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage)
+
+    const products = await ApiFeature.query;
+
+    res.status(200).json({
+        message:"Below is the list of product",
+        success:true,
+        products
+    })
+})
+
+// Get Product Details
+
+exports.getProductDetails = catchAsyncErrors(async (req,res,next)=>{
+    let product = await Product.findById(req.params.id)
+    console.log(req.params.id);
+    if(!product){
+        return next(new ErrorHandler("Product not found with this id",404))
+    }
+
+    res.status(200).json({
+        success:true,
+        massage:"Product found successfully",
+        product
+    })
+
+})
+
+// Update Product -- admin
+
+exports.updateProduct = catchAsyncErrors(async (req,res)=>{
+    let product = await  Product.findById(req.params.id)
+
+    if(!product){
+        return next(new ErrorHandler("Product not found with this id",404))
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    })
+
+    res.status(201).json({
+        message:"Product Updated Successfully",
+        status:true,
+        product
+    })
+})
+
+// Delete Product
+
+exports.deleteProduct = catchAsyncErrors(async (req,res,next)=>{
+    const product = await Product.findById(req.params.id)
+
+    if(!product){
+        return next(new ErrorHandler("Product not found with this id",404))
+    }
+
+    await product.remove(); 
+
+    res.status(200).json({
+        success:true,
+        message:"Product delected successfully"
+    })
+})
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const app = require("./app");
+const dotenv = require("dotenv")
+const connectDatabase = require("./config/database")
+
+// handling uncaught exception
+process.on("uncaughtException",(err)=>{
+    console.log(`Error : ${err.message}`)
+    console.log("Shutting down the server due to uncaught exception");
+
+    process.exit(1)
+})
+
+//config
+dotenv.config({path:"backend/config/config.env"})
+
+// connecting to database
+connectDatabase() 
+
+// starting server
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server started and working on ${process.env.PORT}`);
+})
+
+// unhendaled Promise Rejection
+process.on("unhandledRejection",err=>{
+    console.log(`Error : ${err.message}`);
+    console.log("Shutting down the server due to unhendaled promise rejection");
+
+    server.close(()=>{
+        process.exit(1)
+    })
+})
